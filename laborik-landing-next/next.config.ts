8768fbe1fa60dbd26ac9cebfe78045a65615c027
Danel Torchiari <danel.t@hotmail.com>
2025-11-09 20:23:25 -0300

    next config arreglado

--- laborik-landing-next/next.config.ts
+++ laborik-landing-next/next.config.ts
@@ -3,11 +3,11 @@ import { join } from "path";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  outputFileTracingRoot: join(__dirname),
+
   experimental: {
-    turbopack: {
-      root: join(__dirname), // Usa la ruta absoluta correcta
-    },
-    outputFileTracingRoot: join(__dirname), // Hace que coincidan ambos
+    
   },
 };
 
