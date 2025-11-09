93576c63473efa721a129200dc59c7a88bd9547b
Danel Torchiari <danel.t@hotmail.com>
2025-11-09 20:04:33 -0300

    ahora si deploy

--- laborik-landing-next/next.config.ts
+++ laborik-landing-next/next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  turbopack: {
+    // Opcional, solo si querés especificar el root correctamente
+    root: './',
+  },
 };
 
 export default nextConfig;
